edoburu/django-debugtools

commit 94ce219809511ad90066c1a734bf130c5eabd42a

Support printing expressions too with `{% print %}`

debugtools/templatetags/debug_tags.py
@@ -22,10 +22,17 @@ import re
 register = Library()
 
 class PrintNode(Node):
-    def __init__(self, varnames):
+    @classmethod
+    def parse(cls, parser, token):
+        varnames = token.contents.split()[1:]
+        return cls(
+            variables=((name, parser.compile_filter(name)) for name in varnames)
+        )
+
+    def __init__(self, variables):
         # Thread safety OK: the list of varnames won't change for this node.
         # Data is read only inside the render() function.
-        self.variables = dict( (v,Variable(v)) for v in varnames )
+        self.variables = variables
 
     def render(self, context):
         if self.variables:
@@ -44,7 +51,7 @@ class PrintNode(Node):
 
     def print_variables(self, context):
         text = []
-        for name, var in self.variables.iteritems():
+        for name, var in self.variables:
             data = var.resolve(context)
             textdata = linebreaksbr(escape(_dump_var(data)))
 
@@ -62,7 +69,7 @@ def _print(parser, token):
     A template tag which prints dumps the contents of objects.
     """
     varnames = token.contents.split()
-    return PrintNode(varnames[1:])
+    return PrintNode.parse(parser, token)
 
 
 @register.inclusion_tag('debugtools/sql_queries.html', takes_context=True)

commit 5f5f6ee091b24699d22517a61bb75197bd4090ec

Fix `{% print %}` without variables.
Generator expression returns True, needs to be a list.
Fixes: 94ce219809511ad90066c1a734bf130c5eabd42a

debugtools/templatetags/debug_tags.py
@@ -27,13 +27,13 @@ class PrintNode(Node):
     def parse(cls, parser, token):
         varnames = token.contents.split()[1:]
         return cls(
-            variables=((name, parser.compile_filter(name)) for name in varnames)
+            variables=[(name, parser.compile_filter(name)) for name in varnames]
         )
 
     def __init__(self, variables):
         # Thread safety OK: the list of varnames won't change for this node.
         # Data is read only inside the render() function.
-        self.variables = variables
+        self.variables = list(variables)
 
     def render(self, context):
         if self.variables:
@@ -69,7 +69,6 @@ def _print(parser, token):
     """
     A template tag which prints dumps the contents of objects.
     """
-    varnames = token.contents.split()
     return PrintNode.parse(parser, token)
 
 