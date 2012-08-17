34f71a3a77c49c0ce4210ac7565ead4a9f71763e
Diederik van der Boor <vdboor@edoburu.nl>
2012-08-17 10:26:50 +0200

    Use unicode strings for printing template variables

--- debugtools/templatetags/debug_tags.py
+++ debugtools/templatetags/debug_tags.py
@@ -20,7 +20,7 @@ from pprint import pformat
 import re
 
 # Twitter bootstrap <pre> style:
-PRE_STYLE = """clear: both; font-family: Menlo,Monaco,"Courier new",monospace; color: #333; background-color: #f5f5f5; border: 1px solid rgba(0, 0, 0, 0.15); border-radius: 4px 4px 4px 4px; font-size: 12.025px; line-height: 18px; margin: 9px; padding: 8px;"""
+PRE_STYLE = u"""clear: both; font-family: Menlo,Monaco,"Courier new",monospace; color: #333; background-color: #f5f5f5; border: 1px solid rgba(0, 0, 0, 0.15); border-radius: 4px 4px 4px 4px; font-size: 12.025px; line-height: 18px; margin: 9px; padding: 8px;"""
 
 
 register = Library()
@@ -45,13 +45,13 @@ class PrintNode(Node):
             return self.print_context(context)
 
     def print_context(self, context):
-        text = ['<h6 style="color: #999999; font-size: 11px; margin: 9px 0;">TEMPLATE CONTEXT SCOPE:</h6>\n']
+        text = [u'<h6 style="color: #999999; font-size: 11px; margin: 9px 0;">TEMPLATE CONTEXT SCOPE:</h6>\n']
         for i, part in enumerate(context):
-            code = "<pre style='{style}'>" \
-                   "<small><a href='#' onclick='var s1=this.parentNode.nextSibling, s2=s1.nextSibling, d1=s1.style.display, d2=s2.style.display; s1.style.display=d2; s2.style.display=d1; return false'>{num}:</a> </small>" \
-                   "<span>{dump1}</span><span style='display:none'>{dump2}</span></pre>"
+            code = u"<pre style='{style}'>" \
+                   u"<small><a href='#' onclick='var s1=this.parentNode.nextSibling, s2=s1.nextSibling, d1=s1.style.display, d2=s2.style.display; s1.style.display=d2; s2.style.display=d1; return false'>{num}:</a> </small>" \
+                   u"<span>{dump1}</span><span style='display:none'>{dump2}</span></pre>"
             text.append(code.format(style=PRE_STYLE, num=i, dump1=linebreaksbr(escape(_dump_var(part))), dump2=escape(_dict_summary(part))))
-        return mark_safe(''.join(text))
+        return mark_safe(u''.join(text))
 
     def print_variables(self, context):
         text = []
@@ -64,16 +64,16 @@ class PrintNode(Node):
                 else:
                     data = expr.resolve(context)  # could return TEMPLATE_STRING_IF_INVALID
             except VariableDoesNotExist as e:
-                textdata = '<span style="color: #B94A48;">{0}</span>'.format(escape('<{0}>'.format(e)))
+                textdata = u'<span style="color: #B94A48;">{0}</span>'.format(escape('<{0}>'.format(e)))
             else:
                 textdata = linebreaksbr(escape(_dump_var(data)))
 
             # At top level, prefix class name if it's a longer result
             if isinstance(data, (bool,int,basestring,float, Promise)):
-                text.append("<pre style='{0}'>{1} = {2}</pre>".format(PRE_STYLE, name, textdata))
+                text.append(u"<pre style='{0}'>{1} = {2}</pre>".format(PRE_STYLE, name, textdata))
             else:
-                text.append("<pre style='{0}'>{1} = {2}...\n{3}</pre>".format(PRE_STYLE, name, data.__class__.__name__, textdata))
-        return mark_safe(''.join(text))
+                text.append(u"<pre style='{0}'>{1} = {2}...\n{3}</pre>".format(PRE_STYLE, name, data.__class__.__name__, textdata))
+        return mark_safe(u''.join(text))
 
 
 @register.tag('print')
@@ -99,8 +99,8 @@ RE_SQL = re.compile(r'\b(SELECT|UPDATE|DELETE'
 @register.filter
 def format_sql(sql):
     sql = escape(sql)
-    sql = RE_SQL_NL.sub('<br>\n\\1', sql)
-    sql = RE_SQL.sub('<strong>\\1</strong>', sql)
+    sql = RE_SQL_NL.sub(u'<br>\n\\1', sql)
+    sql = RE_SQL.sub(u'<strong>\\1</strong>', sql)
     return mark_safe(sql)
 
 
@@ -175,7 +175,7 @@ def _dump_var(object):
         try:
             text = pformat(object)
         except Exception, e:
-            return "<caught %s while rendering: %s>" % (e.__class__.__name__, str(e) or "<no exception message>")
+            return u"<caught %s while rendering: %s>" % (e.__class__.__name__, str(e) or "<no exception message>")
 
         text = text.replace("<django.utils.functional.__proxy__ object", '<proxy object')
         return text
@@ -196,7 +196,7 @@ def _format_value(value):
         return str(value) + "???"
     elif isinstance(value, Node):
         # The Block node is very verbose, making debugging hard.
-        return "<Block Node: %s, ...>" % value.name
+        return u"<Block Node: %s, ...>" % value.name
     elif isinstance(value, Promise):
         # lazy() object
         return _format_lazy(value)
