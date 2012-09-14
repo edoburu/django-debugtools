e028fc708f390ab1d24420a2bb360f6711bd1d96
Diederik van der Boor <vdboor@edoburu.nl>
2012-09-14 11:20:07 +0200

    Enhance X-View-Template to show the exact template being used.

--- debugtools/middleware/xviewmiddleware.py
+++ debugtools/middleware/xviewmiddleware.py
@@ -1,5 +1,7 @@
 from django.conf import settings
-from django import http
+from django.template import TemplateDoesNotExist
+from django.template.loader import get_template
+
 
 class XViewMiddleware(object):
     """
@@ -16,12 +18,51 @@ class XViewMiddleware(object):
             "The XView middleware requires authentication middleware to be "
             "installed. Edit your MIDDLEWARE_CLASSES setting to insert "
             "'django.contrib.auth.middleware.AuthenticationMiddleware'.")
+
         if request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS or (request.user.is_active and request.user.is_staff):
             request._xview = "{0}.{1}".format(view_func.__module__, view_func.__name__)
+
 
     def process_response(self, request, response):
         if hasattr(request, '_xview'):
             response['X-View'] = request._xview
+
         if hasattr(response, 'template_name'):
-            response['X-View-Template'] = response.template_name
+            template = response.template_name
+
+            if isinstance(template, (list, tuple)):
+                # See which template name was really used.
+                if len(template) == 1:
+                    response['X-View-Template'] = template[0]
+                else:
+                    used_name = _get_used_template_name(template)
+                    response['X-View-Template'] = '{0}   (out of: {1})'.format(used_name, ', '.join(template))
+            elif isinstance(template, basestring):
+                # Single string
+                response['X-View-Template'] = template
+            else:
+                # Template object.
+                filename = _get_template_filename(template)
+                response['X-View-Template'] = '<template object from {0}>'.format(filename) if filename else '<template object>'
+
         return response
+
+
+def _get_used_template_name(template_name_list):
+    """
+    Find which template of the template_names is selected by the Django loader.
+    """
+    for template_name in template_name_list:
+        try:
+            get_template(template_name)
+            return template_name
+        except TemplateDoesNotExist:
+            continue
+
+
+def _get_template_filename(template):
+    # With TEMPLATE_DEBUG = True, each node tracks it's origin.
+    try:
+        return template.nodelist[0].origin[0].name
+    except (AttributeError, IndexError):
+        return None
